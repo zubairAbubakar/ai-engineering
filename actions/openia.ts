d344f7b214a01f2d62592944abc8ed54c28e1e94
Zubair Abubakar <zuby52@gmail.com>
2025-02-15 21:04:48 +0000

    setup openi api

---
+++ actions/openia.ts
@@ -0,0 +1,25 @@
+import OpenAI from 'openai';
+
+export async function getOpenaiResponse(question: string) {
+  const openai = new OpenAI({
+    dangerouslyAllowBrowser: true,
+  });
+
+  const messages: OpenAI.Chat.ChatCompletionMessageParam[] = [
+    {
+      role: 'system',
+      content: 'You are a helpful general knowledge expert.',
+    },
+    {
+      role: 'user',
+      content: question,
+    },
+  ];
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: messages,
+  });
+
+  return response.choices[0].message.content;
+}
